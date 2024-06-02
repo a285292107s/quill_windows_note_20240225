f2e49d21fac32b910459131a074d5821b5087f99
大白兔奶糖 <285292107@qq.com>
2024-06-02 18:34:12 +0800

    修复了窗口最大化/还原问题，并添加了最小化、关闭按钮

--- lib/inbox.dart
+++ lib/inbox.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:window_manager/window_manager.dart';
 
 class SendNotePage extends StatefulWidget {
   @override
@@ -13,7 +14,8 @@ class _SendNotePageState extends State<SendNotePage> {
   String _responseBody = '';
 
   Future<void> sendNoteToInbox() async {
-    final String url = 'https://app.gudong.site/api/inbox/bYzIBejD8qBkjY62Wi3cQUIfgmDLHm';
+    final String url =
+        'https://app.gudong.site/api/inbox/bYzIBejD8qBkjY62Wi3cQUIfgmDLHm';
     final Map<String, String> headers = {
       'Content-Type': 'application/json',
     };
@@ -38,11 +40,50 @@ class _SendNotePageState extends State<SendNotePage> {
     }
   }
 
+  bool isMaximized = false;
+
+  @override
+  void initState() {
+    super.initState();
+    checkMaximizedStatus();
+  }
+
+  void checkMaximizedStatus() async {
+    isMaximized = await windowManager.isMaximized();
+    setState(() {});
+  }
+
+  void toggleMaximize() async {
+    isMaximized ? windowManager.restore() : windowManager.maximize();
+    isMaximized = !isMaximized;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Send to inBox'),
+        title: Text('Notes'),
+        //leading: Icon(Icons.menu),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.minimize),
+            onPressed: () {
+              windowManager.minimize();
+            },
+          ),
+          IconButton(
+            //没找到还原按钮，先用同一个按钮凑合着
+            icon: Icon(isMaximized ? Icons.crop_square : Icons.crop_square),
+            onPressed: toggleMaximize,
+          ),
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              windowManager.close();
+            },
+          ),
+        ],
       ),
       body: Column(
         children: <Widget>[
@@ -51,6 +92,8 @@ class _SendNotePageState extends State<SendNotePage> {
               padding: const EdgeInsets.all(16.0),
               child: TextField(
                 controller: _controller,
+                maxLines: null,
+                minLines: 99,
                 decoration: InputDecoration(
                   hintText: 'Enter your note here',
                   border: OutlineInputBorder(),
@@ -59,7 +102,8 @@ class _SendNotePageState extends State<SendNotePage> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
+            padding:
+                const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
             child: ElevatedButton(
               onPressed: sendNoteToInbox,
               child: Text('Send'),
